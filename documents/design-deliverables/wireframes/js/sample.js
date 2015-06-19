32076e28f4844dccc75ebf5667b93aa7039bfef9
Greg Tarnoff <greg.tarnoff@gmail.com>
2015-06-19 15:49:54 -0500

    resolved errors D3 was throwing as well as missing JS files

--- documents/design-deliverables/wireframes/js/sample.js
+++ documents/design-deliverables/wireframes/js/sample.js
@@ -2,22 +2,22 @@
 "use strict";
 console.log('loaded sample.js');
   var data1 = [4, 8 ,15, 16, 23, 42],
-  data2 =[15, 34, 29, 12, 123, 1],
-  data3 = [9, 287, 2, 942, 234],
-  data4 = [872, 27834, 8713, 234];
-  var width = '100%',
-  barHeight = 20;
+  data2 =[1, 4, 9, 12, 8],
+  data3 = [9, 8, 2, 3, 7],
+  data4 = [7, 8, 3, 5, 4];
+  var width = 780,
+  barHeight = 40;
 
   var x = d3.scale.linear()
     .domain([0, d3.max(data1)])
     .range([0, width]);
   var chart = d3.select('.chart')
-    .attr('width', width)
-    .attr('height', barHeight * data1.length);
+    .attr('width', width+'%')
+    .attr('height', barHeight * data1.length * 2);
   var bar = chart.selectAll('g')
       .data(data1)
       .enter().append('g')
-      .attr('transform', function(d, i) {return "translate(0,"+ i *barHeight + ")"; });
+      .attr('transform', function(d, i) {return "translate(0,"+ i * barHeight * 2 + ")"; });
   bar.append("rect")
     .attr('width', x)
     .attr('height', barHeight -1);
@@ -27,7 +27,8 @@ console.log('loaded sample.js');
     .attr('dy', '.35em')
     .text(function(d) {return d;});
 
-
+  var width = 220,
+    barHeight = 20;
   var chart = d3.select('.chart2')
     .attr('width', width)
     .attr('height', barHeight * data1.length);
